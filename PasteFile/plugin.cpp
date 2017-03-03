1c4dd70dc22b9dc876ae0cde756b9f164218144b
mrexodia <mr.exodia.tpodt@gmail.com>
2017-03-03 13:10:38 +0100

    initial commit

---
+++ PasteFile/plugin.cpp
@@ -0,0 +1,69 @@
+#include "plugin.h"
+
+static std::wstring Utf8ToUtf16(const std::string & str)
+{
+    std::wstring convertedString;
+    int requiredSize = MultiByteToWideChar(CP_UTF8, 0, str.c_str(), -1, nullptr, 0);
+    if(requiredSize > 0)
+    {
+        std::vector<wchar_t> buffer(requiredSize);
+        MultiByteToWideChar(CP_UTF8, 0, str.c_str(), -1, &buffer[0], requiredSize);
+        convertedString.assign(buffer.begin(), buffer.end() - 1);
+    }
+    return convertedString;
+}
+
+static bool ReadAllData(const std::string & fileName, std::vector<uint8_t> & content)
+{
+    auto hFile = CreateFileW(Utf8ToUtf16(fileName).c_str(), GENERIC_READ, FILE_SHARE_READ, nullptr, OPEN_EXISTING, 0, nullptr);
+    auto result = false;
+    if(hFile != INVALID_HANDLE_VALUE)
+    {
+        unsigned int filesize = GetFileSize(hFile, nullptr);
+        if(!filesize)
+        {
+            content.clear();
+            result = true;
+        }
+        else
+        {
+            content.resize(filesize);
+            DWORD read = 0;
+            result = !!ReadFile(hFile, content.data(), filesize, &read, nullptr);
+        }
+        CloseHandle(hFile);
+    }
+    return result;
+}
+
+static bool cbCommand(int argc, char* argv[])
+{
+    if(argc < 2)
+        return false;
+    std::vector<uint8_t> data;
+    if(!ReadAllData(argv[1], data))
+        return false;
+    auto start = Script::Gui::Dump::SelectionGetStart();
+    DbgFunctions()->MemPatch(start, data.data(), data.size());
+    GuiUpdateAllViews();
+    return true;
+}
+
+//Initialize your plugin data here.
+bool pluginInit(PLUG_INITSTRUCT* initStruct)
+{
+    if(!_plugin_registercommand(pluginHandle, PLUGIN_NAME, cbCommand, false))
+        _plugin_logputs("[" PLUGIN_NAME "] Error registering the \"" PLUGIN_NAME "\" command!");
+    return true; //Return false to cancel loading the plugin.
+}
+
+//Deinitialize your plugin data here (clearing menus optional).
+bool pluginStop()
+{
+    return true;
+}
+
+//Do GUI/Menu related things here.
+void pluginSetup()
+{
+}
